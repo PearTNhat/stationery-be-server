32e0a0d0042d33899cd4915360154799a903d8e3
quockhoa53 <nguyenquockhoa5549@gmail.com>
2025-03-28 16:16:00 +0700

    Merge, Logout lưu token, Thay đổi mật khẩu

--- src/main/java/com/project/stationery_be_server/entity/ProductDetail.java
+++ src/main/java/com/project/stationery_be_server/entity/ProductDetail.java
@@ -1,5 +1,6 @@
 package com.project.stationery_be_server.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
