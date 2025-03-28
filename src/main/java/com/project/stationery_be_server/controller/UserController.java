c1bdcba61c63c4dcc318348d221d0eb1aa63d3ec
quockhoa53 <nguyenquockhoa5549@gmail.com>
2025-03-28 16:16:00 +0700

    Merge, Logout lưu token, Thay đổi mật khẩu

--- src/main/java/com/project/stationery_be_server/controller/UserController.java
+++ src/main/java/com/project/stationery_be_server/controller/UserController.java
@@ -72,5 +72,4 @@ public class UserController {
                 .result(message)
                 .build();
     }
-
 }
--- src/main/java/com/project/stationery_be_server/entity/Category.java
+++ src/main/java/com/project/stationery_be_server/entity/Category.java
@@ -1,5 +1,7 @@
 package com.project.stationery_be_server.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,14 +12,21 @@ import java.util.List;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "category_id")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private String category_id;
 
-    @Column(length = 30, nullable = false,unique = true)
+    @Column(length = 30, nullable = false, unique = true)
     private String category_name;
 
-    @OneToMany(mappedBy = "category")
+    @Column(length = 10, nullable = false)
+    private String icon;
+
+    @Column(length = 20, nullable = false)
+    private String bg_color;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
     private List<Product> products;
 }
