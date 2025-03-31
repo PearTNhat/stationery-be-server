549e57ec6944ad1c685e7680c848d31945059043
levi <leetuan0342@gmail.com>
2025-03-31 23:24:30 +0700

    update user, create update address, create update color, create purchase order, get vouncher

---
+++ src/main/java/com/project/stationery_be_server/dto/response/PromotionResponse.java
@@ -0,0 +1,22 @@
+package com.project.stationery_be_server.dto.response;
+
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PromotionResponse {
+    private String promotionId;
+    private String promoCode;
+    private String discountType;
+    private BigDecimal discountValue;
+    private BigDecimal maxValue;
+    private BigDecimal minOrderValue;
+    private LocalDate startDate;
+    private LocalDate endDate;
+}
--- src/main/java/com/project/stationery_be_server/entity/PurchaseOrderDetailId.java
+++ src/main/java/com/project/stationery_be_server/entity/PurchaseOrderDetailId.java
@@ -2,10 +2,16 @@ package com.project.stationery_be_server.entity;
 
 import jakarta.persistence.Embeddable;
 import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Embeddable
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PurchaseOrderDetailId {
     @Column(name = "purchase_order_id")
     private String purchaseOrderId;
