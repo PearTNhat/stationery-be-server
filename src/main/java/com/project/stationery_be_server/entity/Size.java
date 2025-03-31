e81e6d2fac1f7c89f1d8541e8ec811947e3e24cd
Hai <nguyenvuhai60@gmail.com>
2025-03-31 22:43:16 +0700

    Create&UpdateSize, Create&Update&RemoveCart

--- src/main/java/com/project/stationery_be_server/entity/Size.java
+++ src/main/java/com/project/stationery_be_server/entity/Size.java
@@ -17,11 +17,14 @@ import java.util.Set;
 @Entity
 public class Size {
     @Id
-    @Column(name = "size_id", length = 10) // Giữ snake_case trong DB
+    @Column(name = "size_id", length = 10) //
     String sizeId;
 
     @Column(name = "name", nullable = false, length = 3, unique = true)
     String name;
+
+    @Column(name = "priority", nullable = false, length = 3, unique = true)
+    String priority;
 
     @OneToMany(mappedBy = "size", fetch = FetchType.LAZY)
     @JsonIgnore
